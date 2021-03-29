2e2a6b47cfb8720922525be1126c47d2eda7019b
Luca Gallinari <luke.gallinari@gmail.com>
2021-03-29 11:52:29 +0200

    Compute "templates dir" template files changes

--- src/Command/TemplateChangesCommand.php
+++ src/Command/TemplateChangesCommand.php
@@ -5,23 +5,138 @@ declare(strict_types=1);
 namespace Webgriffe\SyliusUpgradePlugin\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Webmozart\Glob\Glob;
 
 final class TemplateChangesCommand extends Command
 {
+    public const FROM_VERSION_ARGUMENT_NAME = 'from';
+
+    public const TO_VERSION_ARGUMENT_NAME = 'to';
+
+    private const TEMPLATES_BUNDLES_SUBDIR = 'templates/bundles/';
+
     protected static $defaultName = 'webgriffe:upgrade:template-changes';
+
+    /** @var string */
+    private $rootPath;
+
+    /** @var OutputInterface */
+    private $output;
+
+    public function __construct(string $rootPath, string $name = null)
+    {
+        parent::__construct($name);
+
+        $this->rootPath = rtrim($rootPath, \DIRECTORY_SEPARATOR) . \DIRECTORY_SEPARATOR;
+    }
 
     protected function configure(): void
     {
-        $this->setDescription(
-            'Print a list of template files (with extension .html.twig) that changed between two given Sylius versions ' .
-            'and that has been overridden in the project (in "templates" dir or in a theme).'
-        );
+        $this
+            ->setDescription(
+                'Print a list of template files (with extension .html.twig) that changed between two given Sylius versions ' .
+                'and that has been overridden in the project (in "templates" dir or in a theme).'
+            )
+            ->addArgument(
+                self::FROM_VERSION_ARGUMENT_NAME,
+                InputArgument::REQUIRED,
+                'Starting Sylius version to use for changes computation.'
+            )
+            ->addArgument(
+                self::TO_VERSION_ARGUMENT_NAME,
+                InputArgument::REQUIRED,
+                'Target Sylius version to use for changes computation.'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $this->output = $output;
+
+        $fromVersion = $input->getArgument(self::FROM_VERSION_ARGUMENT_NAME);
+        if (!is_string($fromVersion)) {
+            // todo
+            return 1;
+        }
+        $toVersion = $input->getArgument(self::TO_VERSION_ARGUMENT_NAME);
+        if (!is_string($toVersion)) {
+            // todo
+            return 1;
+        }
+        $versionChangedFiles = $this->getFilesChangedBetweenTwoVersions($fromVersion, $toVersion);
+        $this->computeTemplateFilesChangedAndOverridden($versionChangedFiles);
+
+        // todo: compute theme files
+
         return 0;
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getFilesChangedBetweenTwoVersions(string $fromVersion, string $toVersion): array
+    {
+        $url = 'https://github.com/Sylius/Sylius/compare/v' . $fromVersion . '...v' . $toVersion . '.diff';
+        $this->output->writeln(sprintf('Computing differences between %s and %s: %s', $fromVersion, $toVersion, $url));
+        $diff = @file_get_contents($url);
+        if ($diff === false) {
+            $this->output->writeln('Error: one or both given versions does not exists.');
+            exit(1);
+        }
+        $versionChangedFiles = [];
+        $diffLines = explode(\PHP_EOL, $diff);
+        foreach ($diffLines as $diffLine) {
+            if (strpos($diffLine, 'diff --git') !== 0) {
+                continue;
+            }
+            $diffLineParts = explode(' ', $diffLine);
+            $changedFileName = substr($diffLineParts[2], 2);
+            if (strpos($changedFileName, 'Resources/views') === false) {
+                continue;
+            }
+            $versionChangedFiles[] = $changedFileName;
+        }
+
+        // src/Sylius/Bundle/AdminBundle/Resources/views/PaymentMethod/_form.html.twig -> SyliusAdminBundle/PaymentMethod/_form.html.twig
+        return array_map(
+            static function (string $versionChangedFile): string {
+                return str_replace(['src/Sylius/Bundle/', '/Resources/views'], ['Sylius', ''], $versionChangedFile);
+            },
+            $versionChangedFiles
+        );
+    }
+
+    private function computeTemplateFilesChangedAndOverridden(array $versionChangedFiles): void
+    {
+        $targetDir = $this->rootPath . self::TEMPLATES_BUNDLES_SUBDIR;
+        $this->output->writeln('');
+        $this->output->writeln(sprintf('Searching "%s" for overridden files that changed between the two versions.', $targetDir));
+        $templateFilenames = $this->getProjectTemplatesFiles($targetDir);
+        /** @var string[] $overriddenTemplateFiles */
+        $overriddenTemplateFiles = array_intersect($versionChangedFiles, $templateFilenames);
+        $this->output->writeln(sprintf('Found %s files that changed and was overridden:', count($overriddenTemplateFiles)));
+        foreach ($overriddenTemplateFiles as $file) {
+            $this->output->writeln("\t" . $file);
+        }
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getProjectTemplatesFiles(string $targetDir): array
+    {
+        $files = Glob::glob($targetDir . 'Sylius*Bundle/**/' . '*.html.twig');
+
+        // from /Users/user/workspace/project/templates/bundles/SyliusAdminBundle/PaymentMethod/_form.html.twig
+        // to SyliusAdminBundle/PaymentMethod/_form.html.twig
+        return array_map(
+            static function (string $file) use ($targetDir): string {
+                return str_replace($targetDir, '', $file);
+            },
+            $files
+        );
     }
 }
--- tests/Integration/Command/TemplateChangesCommandTest.php
+++ tests/Integration/Command/TemplateChangesCommandTest.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Tests\Webgriffe\SyliusUpgradePlugin\Integration\Command;
 
+use org\bovigo\vfs\vfsStream;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
+use Webgriffe\SyliusUpgradePlugin\Command\TemplateChangesCommand;
 
 final class TemplateChangesCommandTest extends KernelTestCase
 {
+
     /** @var CommandTester */
     private $commandTester;
 
@@ -17,6 +20,7 @@ final class TemplateChangesCommandTest extends KernelTestCase
     {
         parent::setUp();
         self::bootKernel();
+        vfsStream::setup();
 
         $application = new Application(static::$kernel);
         $command = $application->find('webgriffe:upgrade:template-changes');
@@ -30,5 +34,32 @@ final class TemplateChangesCommandTest extends KernelTestCase
     {
         $return = $this->commandTester->execute([]);
         self::assertEquals(0, $return);
+    }
+
+    /**
+     * @test
+     */
+    public function it_outputs_filepaths_of_overridden_template_files_that_changed_between_two_given_versions(): void
+    {
+        vfsStream::copyFromFileSystem(__DIR__ . '/../DataFixtures/Command/TemplateChangesCommandTest/it_outputs_filepaths_of_overridden_template_files_that_changed_between_two_given_versions/');
+
+        $return = $this->commandTester->execute([
+            TemplateChangesCommand::FROM_VERSION_ARGUMENT_NAME => '1.8.4',
+            TemplateChangesCommand::TO_VERSION_ARGUMENT_NAME => '1.8.8',
+        ]);
+
+        self::assertEquals(0, $return);
+        $output = $this->commandTester->getDisplay();
+        $expectedOutput = <<<TXT
+Computing differences between 1.8.4 and 1.8.8: https://github.com/Sylius/Sylius/compare/v1.8.4...v1.8.8.diff
+
+Searching "vfs://root/templates/bundles/" for overridden files that changed between the two versions.
+Found 2 files that changed and was overridden:
+	SyliusShopBundle/Checkout/Address/_form.html.twig
+	SyliusUiBundle/Form/theme.html.twig
+
+TXT;
+
+        self::assertEquals($expectedOutput, $output);
     }
 }
