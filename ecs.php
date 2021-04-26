a9455eba0efa63ce18af94c4dc265f13372bc22f
Luca Gallinari <luke.gallinari@gmail.com>
2021-04-26 11:45:58 +0200

    Run ecs in github actions

--- ecs.php
+++ ecs.php
@@ -1,7 +1,11 @@
 <?php
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [__DIR__ . '/src']);
 };
