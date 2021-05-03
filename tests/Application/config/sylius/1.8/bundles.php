f7b66747fe7a170671d10923f5ba38a4f7203fd8
Luca Gallinari <luke.gallinari@gmail.com>
2021-05-03 11:36:43 +0200

    Load different ConsoleColorDiffBundle class based on class existency

--- tests/Application/config/sylius/1.8/bundles.php
+++ tests/Application/config/sylius/1.8/bundles.php
@@ -3,4 +3,5 @@
 return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
+    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
 ];
--- tests/Application/config/sylius/1.9/bundles.php
+++ tests/Application/config/sylius/1.9/bundles.php
@@ -1,6 +1,10 @@
 <?php
 
+$consoleColorClass = class_exists('Symplify\ConsoleColorDiff\ConsoleColorDiffBundle') ?
+    'Symplify\ConsoleColorDiff\ConsoleColorDiffBundle' :
+    'Symplify\ConsoleColorDiff\Bundle\ConsoleColorDiffBundle';
 return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    $consoleColorClass => ['dev' => true, 'test' => true],
 ];
