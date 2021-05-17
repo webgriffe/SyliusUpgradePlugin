c6e8628e85cf10163ca4c4deb6b5446d8b2d62de
Luca Gallinari <luke.gallinari@gmail.com>
2021-05-17 10:27:00 +0200

    Remove unnecessary symfony color bundle

--- tests/Application/config/sylius/1.8/bundles.php
+++ tests/Application/config/sylius/1.8/bundles.php
@@ -2,6 +2,5 @@
 
 return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
-    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
+    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true]
 ];
