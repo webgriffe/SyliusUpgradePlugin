0d57df07904a89f567ddd917b6fbe5698d501fe1
Luca Gallinari <luke.gallinari@gmail.com>
2023-11-27 10:41:03 +0100

    Test directly decorated services

---
+++ tests/Stub/ServiceChangesCommand/test_it_detects_directly_decorated_services_that_changed/DecorateOrderEmailManagerInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Webgriffe\SyliusUpgradePlugin\Stub\ServiceChangesCommand\test_it_detects_directly_decorated_services_that_changed;
+
+final class DecorateOrderEmailManagerInterface
+{
+}
