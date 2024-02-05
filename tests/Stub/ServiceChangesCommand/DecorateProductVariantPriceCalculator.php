2cb2f5b2aafb0ce87cddd20c0fc08506783eac90
Luca Gallinari <luke.gallinari@gmail.com>
2024-02-05 12:27:47 +0100

    Specify interfaces for decorated methods in tests

--- tests/Stub/ServiceChangesCommand/DecorateProductVariantPriceCalculator.php
+++ tests/Stub/ServiceChangesCommand/DecorateProductVariantPriceCalculator.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace Tests\Webgriffe\SyliusUpgradePlugin\Stub\ServiceChangesCommand;
 
-final class DecorateProductVariantPriceCalculator
+use Sylius\Component\Core\Calculator\ProductVariantPricesCalculatorInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+
+final class DecorateProductVariantPriceCalculator implements ProductVariantPricesCalculatorInterface
 {
+    public function calculate(ProductVariantInterface $productVariant, array $context): int
+    {
+        return 0;
+    }
+
+    public function calculateOriginal(ProductVariantInterface $productVariant, array $context): int
+    {
+        return 0;
+    }
 }
--- tests/Stub/ServiceChangesCommand/test_it_detects_with_decorated_definition_strategy_those_decorated_services_that_changed/DecorateProvinceNamingProvider.php
+++ tests/Stub/ServiceChangesCommand/test_it_detects_with_decorated_definition_strategy_those_decorated_services_that_changed/DecorateProvinceNamingProvider.php
@@ -4,11 +4,23 @@ declare(strict_types=1);
 
 namespace Tests\Webgriffe\SyliusUpgradePlugin\Stub\ServiceChangesCommand\test_it_detects_with_decorated_definition_strategy_those_decorated_services_that_changed;
 
+use Sylius\Component\Addressing\Model\AddressInterface;
 use Sylius\Component\Addressing\Provider\ProvinceNamingProvider as BaseProvinceNamingProvider;
+use Sylius\Component\Addressing\Provider\ProvinceNamingProviderInterface;
 
-final class DecorateProvinceNamingProvider
+final class DecorateProvinceNamingProvider implements ProvinceNamingProviderInterface
 {
     public function __construct(private BaseProvinceNamingProvider $baseProvinceNamingProvider)
     {
+    }
+
+    public function getName(AddressInterface $address): string
+    {
+        return $this->baseProvinceNamingProvider->getName($address);
+    }
+
+    public function getAbbreviation(AddressInterface $address): string
+    {
+        return $this->baseProvinceNamingProvider->getAbbreviation($address);
     }
 }
