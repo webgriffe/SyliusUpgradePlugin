43ffd7be4baf4e3e61080c3ec79b045778b42053
Luca Gallinari <luke.gallinari@gmail.com>
2024-02-05 12:27:47 +0100

    Specify interfaces for decorated methods in tests

--- tests/Stub/ServiceChangesCommand/test_it_detects_with_decorated_definition_strategy_those_decorated_services_that_changed/DecorateCustomerOrderAddressesSaver.php
+++ tests/Stub/ServiceChangesCommand/test_it_detects_with_decorated_definition_strategy_those_decorated_services_that_changed/DecorateCustomerOrderAddressesSaver.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace Tests\Webgriffe\SyliusUpgradePlugin\Stub\ServiceChangesCommand\test_it_detects_with_decorated_definition_strategy_those_decorated_services_that_changed;
 
 use Sylius\Component\Core\Customer\OrderAddressesSaverInterface;
+use Sylius\Component\Core\Model\OrderInterface;
 
-final class DecorateCustomerOrderAddressesSaver
+final class DecorateCustomerOrderAddressesSaver implements OrderAddressesSaverInterface
 {
     public function __construct(private OrderAddressesSaverInterface $decoratedCustomerOrderAddressesSaver)
     {
+    }
+
+    public function saveAddresses(OrderInterface $order): void
+    {
+        $this->decoratedCustomerOrderAddressesSaver->saveAddresses($order);
     }
 }
--- tests/Stub/ServiceChangesCommand/test_it_detects_with_inner_substitution_strategy_those_decorated_services_that_changed/DecorateNewShopBased.php
+++ tests/Stub/ServiceChangesCommand/test_it_detects_with_inner_substitution_strategy_those_decorated_services_that_changed/DecorateNewShopBased.php
@@ -4,6 +4,20 @@ declare(strict_types=1);
 
 namespace Tests\Webgriffe\SyliusUpgradePlugin\Stub\ServiceChangesCommand\test_it_detects_with_inner_substitution_strategy_those_decorated_services_that_changed;
 
-final class DecorateNewShopBased
+use Sylius\Component\Core\Model\Order;
+use Sylius\Component\Order\Context\CartContextInterface;
+use Sylius\Component\Order\Model\OrderInterface;
+use Symfony\Contracts\Service\ResetInterface;
+
+final class DecorateNewShopBased implements CartContextInterface, ResetInterface
 {
+    public function getCart(): OrderInterface
+    {
+        return new Order();
+    }
+
+    public function reset()
+    {
+        // TODO: Implement reset() method.
+    }
 }
