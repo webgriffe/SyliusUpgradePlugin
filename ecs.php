f028e900826ff28c3fbc9045419cbe04501d0874
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2025-05-05 10:46:32 +0200

    Upgrade for Sylius 2.0

--- ecs.php
+++ ecs.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->import('vendor/sylius-labs/coding-standard/ecs.php');
